f9190754b936f5e2220f771256b3307676931995
wellexam <el.zhao.me@gmail.com>
2021-04-17 16:42:28 +0800

    Chinese issue solved

--- class.cpp
+++ class.cpp
@@ -13,12 +13,13 @@ void make(map<int, student>&students, map<int, teacher>&teachers, map<int, staff
     col[2] = "MATH";
     col[3] = "PHY";
     col[4] = "BIO";
+    string name[] = {"尼古拉斯赵四", "张三", "李四", "王二麻子", "郭达斯坦森"};
     for (int i = 0; i <= 4;i++)
     {
         stu_temp[i].card = rand();
         stu_temp[i].sex = rand() % 2;
         stu_temp[i].year = rand();
-        stu_temp[i].name = i;
+        stu_temp[i].name = name[i];
         stu_temp[i].college = col[rand() % 5];
     }
     for (int i = 0; i <= 4;i++)
--- main.cpp
+++ main.cpp
@@ -1,26 +1,42 @@
 #include"class.hpp"
-
+#include<fstream>
 using namespace std;
 using json = nlohmann::json;
 
+static int cnt, pool[100000];
+
 int main()
 {
+    fstream file;
     int input;
-    // map<string, int> name;
-    // map<int, int> card;
-    // map<int, int> seq;
-    map<int, student> students;
-    map<int, teacher> teachers;
-    map<int, staff> staffs;
-    make(students, teachers, staffs);
+    map<string, int> name;//姓名到序号映射
+    map<int, int> card;//学号到序号映射
+    map<int, student> students;//
+    map<int, teacher> teachers;//序号到对象映射
+    map<int, staff> staffs;    //
+    make(students, teachers, staffs);//自动生成三种对象各五个
     json student_js = students;
-    // cout << "欢迎使用高校人员信息管理系统！" << endl
-    //      << "请选择要使用的功能：" << endl
-    //      << "要增加一个新的教师，请输入“1” " << endl
-    //      << "要根据卡号删除成员，请输入“2”" << endl
-    //      << "要根据姓名卡号查找，请输入“3”" << endl
-    //      << "要输出一个学院内的所有教师和学生信息，请输入“4”" << endl
-    //      << "要退出系统，请输入“6”" << endl;
+    json load_js;
+    cout << "欢迎使用高校人员信息管理系统！" << endl
+         << "请选择要使用的功能：" << endl
+         << "要增加一个新的教师，请输入“1” " << endl
+         << "要根据卡号删除成员，请输入“2”" << endl
+         << "要根据姓名卡号查找，请输入“3”" << endl
+         << "要输出一个学院内的所有教师和学生信息，请输入“4”" << endl
+         << "要退出系统，请输入“6”" << endl;
     // auto js = json::parse(student_js);
-    cout << student_js << endl;
+    file.open("data.json", ios::out);
+    cout << "file opened" << endl;
+    file << student_js;
+    cout << "file edited" << endl;
+    file.close();
+    file.open("data.json", ios::in);
+    file >> load_js;
+    map<int, student> recieve = load_js.get<map<int, student>>();
+    int size = recieve.size();
+    for (int i = 0; i < size; i++)
+    {
+        cout << recieve[i].name << endl;
+    }
+    file.close();
 }
